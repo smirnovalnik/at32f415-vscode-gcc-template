cf84c98f5e94e7e2e059610903e727b95e51b9af
Alexander Smirnov <smirnovalnik@yandex.ru>
2024-06-26 18:45:13 +0300

    [fix] systick

--- port/systick.h
+++ port/systick.h
@@ -17,12 +17,8 @@ uint32_t systick_get_tick_sec(void);
 
 uint32_t systick_get_tick_ms(void);
 
-uint64_t systick_get_tick_us(void);
-
 void systick_delay_sec(uint32_t sec);
 
 void systick_delay_ms(uint32_t ms);
-
-void systick_delay_us(uint64_t us);
 
 void systick_interrupt(void);
